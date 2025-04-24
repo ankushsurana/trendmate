e0d8d873bac5f8d5679093d0e35d89d3088adaa0
ankush <ankush.surana@uptiq.ai>
2025-04-25 01:30:03 +0530

    server deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: 'https://ankushsurana.github.io/trendmate/',
   server: {
     host: "::",
     port: 8080,
